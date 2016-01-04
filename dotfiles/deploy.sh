bf6615aed09a3f74f15dd014b093c7e5b4f3256d
yoku0825 <yoku0825@gmail.com>
2016-01-04 12:10:48 +0900

    Support deploy multiple users

--- dotfiles/deploy.sh
+++ dotfiles/deploy.sh
@@ -2,8 +2,19 @@
 
 workdir=$(cd $(dirname $0) ; pwd)
 
-for f in $(ls $workdir/*) ; do
-  [ "$(basename $f)" = "$(basename $0)" ] && continue
-  ln -sf "$f" "$HOME/.$(basename $f)"
-done
+function deploy_for_user
+{
+  local user="$1"
+  for f in $(ls $workdir/*) ; do
+    [ "$(basename $f)" = "$(basename $0)" ] && continue
+    ln -sf "$f" "~$1/.$(basename $f)"
+  done
+}
 
+if [ "$#" = "0" ] ; then
+  deploy_for_user "$USER"
+else
+  for u in $* ; do
+    deploy_for_user $u
+  done
+fi
