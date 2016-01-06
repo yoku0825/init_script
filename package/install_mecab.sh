ff0427afd1b74c1fbd0de5e08c7989922ff2723b
yoku0825 <yoku0825@gmail.com>
2016-01-06 10:46:30 +0900

    Add -y option

--- package/install_mecab.sh
+++ package/install_mecab.sh
@@ -4,4 +4,4 @@ workdir=$(cd $(dirname $0) ; pwd)
 . $workdir/../common.sh
  
 yum install -y http://packages.groonga.org/centos/6/x86_64/Packages/groonga-release-1.1.0-1.noarch.rpm
-yum install mecab mecab-devel mecab-ipadic
+yum install -y mecab mecab-devel mecab-ipadic
