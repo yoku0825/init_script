cc626708ef070497ba98231d3032e8ce1e3bc274
yoku0825 <yoku0825@gmail.com>
2016-01-06 11:55:00 +0900

    Add version handling

--- package/install_mecab.sh
+++ package/install_mecab.sh
@@ -3,5 +3,17 @@
 workdir=$(cd $(dirname $0) ; pwd)
 . $workdir/../common.sh
  
-yum install -y http://packages.groonga.org/centos/6/x86_64/Packages/groonga-release-1.1.0-1.noarch.rpm
+case "$version" in
+  "5")
+    yum install -y http://packages.groonga.org/centos/5/x86_64/Packages/groonga-release-1.1.0-0.noarch.rpm
+    ;;
+  "6"|"amazon")
+    yum install -y http://packages.groonga.org/centos/6/x86_64/Packages/groonga-release-1.1.0-1.noarch.rpm
+    ;;
+  "7"|*)
+    echo "Does not support yet"
+    exit 1;
+    ;;
+esac
+
 yum install -y mecab mecab-devel mecab-ipadic
