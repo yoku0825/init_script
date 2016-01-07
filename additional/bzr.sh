9981d727b6556f95eca09ca407957010b69a79b7
yoku0825 <yoku0825@gmail.com>
2016-01-07 19:57:21 +0900

    Using common.sh

--- additional/bzr.sh
+++ additional/bzr.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-yum install -y tar wget gcc python-devel
-mkdir build
-pushd build
-wget https://launchpad.net/bzr/2.6/2.6.0/+download/bzr-2.6.0.tar.gz
-tar xf bzr-2.6.0.tar.gz
-cd bzr-2.6.0
-python setup.py install
-popd
-rm -r build
+workdir=$(cd $(dirname $0) ; pwd)
+. $workdir/../common.sh
+
+case "$version" in
+  "5")
+    echo "TODO"
+    ;;
+  "6"|"amazon")
+    yum install -y tar wget gcc python-devel
+    mkdir build
+    pushd build
+    wget https://launchpad.net/bzr/2.6/2.6.0/+download/bzr-2.6.0.tar.gz
+    tar xf bzr-2.6.0.tar.gz
+    cd bzr-2.6.0
+    python setup.py install
+    popd
+    rm -r build
+    ;;
+  "7"|*)
+    echo "TODO"
+    ;;
+esac
