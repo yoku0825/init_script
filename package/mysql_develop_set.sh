d5a96593ddf5283b464f8ca7930c7a6f6b8afa99
yoku0825 <yoku0825@gmail.com>
2016-01-08 10:31:14 +0900

    Add version handling

--- package/mysql_develop_set.sh
+++ package/mysql_develop_set.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-yum install -y cmake make gcc gcc-c++ ncurses-devel bison
+case "$version" in
+  "5")
+    echo "TODO"
+    ;;
+  "6"|"amazon")
+    yum install -y cmake make gcc gcc-c++ ncurses-devel bison
+    ;;
+  "7")
+    echo "TODO"
+    ;;
+esac
+
