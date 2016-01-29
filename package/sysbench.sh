833ded4fa91ff33e23299beff70f309cfc7e176a
yoku0825 <yoku0825@gmail.com>
2016-01-29 14:29:53 +0900

    Add sysbench.

---
+++ package/sysbench.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+workdir=$(cd $(dirname $0) ; pwd)
+. $workdir/../common.sh
+ 
+case "$version" in
+  "5")
+    yum install -y http://dev.mysql.com/get/mysql57-community-release-el5-7.noarch.rpm
+    ;;
+  "6"|"amazon")
+    yum install -y http://dev.mysql.com/get/mysql57-community-release-el6-7.noarch.rpm
+    ;;
+  "7")
+    yum install -y http://dev.mysql.com/get/mysql57-community-release-el7-7.noarch.rpm
+    ;;
+esac
+
+yum install -y mysql-community-devel
+git clone https://github.com/akopytov/sysbench.git
+
+pushd sysbench
+  ./autogen.sh
+  ./configure
+  make
+  make install
+popd
