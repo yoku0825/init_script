d9f0ef52d06f54d12e78f62996d3d2f434717473
yoku0825 <yoku0825@gmail.com>
2017-09-26 16:03:17 +0900

    Add epel-release

--- docker/docker_basic.sh
+++ docker/docker_basic.sh
@@ -2,7 +2,7 @@
 
 yum upgrade -y
 ln -sf /usr/share/zoneinfo/Japan /etc/localtime
-yum install -y tar which patch git sudo wget perl
+yum install -y tar which patch git sudo wget perl epel-release
 yum clean all
 
 test -d /tmp/setup || mkdir /tmp/setup
