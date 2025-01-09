1f9b41d7cb884b22b6030ce21afa26a3f707add7
yoku0825 <yoku0825@gmail.com>
2025-01-09 08:50:01 +0000

    Renew global version

--- package/gtags.sh
+++ package/gtags.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-. $(cd $(dirname $0) ; pwd)/pip.sh
-pip install pygments
+#. $(cd $(dirname $0) ; pwd)/pip.sh
+#pip install pygments
 
-yum install -y tar wget make gcc ncurses-devel
+dnf install -y tar wget make gcc ncurses-devel python3-pygments
 mkdir build
-pushd build
+cd build
 
-wget http://tamacom.com/global/global-6.5.2.tar.gz
-tar xzf global-6.5.2.tar.gz
-pushd global-6.5.2
+wget https://ftp.gnu.org/pub/gnu/global/global-6.6.14.tar.gz
+tar xzf global-6.6.14.tar.gz
+cd global-6.6.14
 ./configure --prefix=/usr
 make
 make install
 mkdir -p ~/.vim/plugin/
 cp /usr/share/gtags/gtags.vim ~/.vim/plugin/gtags.vim
-popd
+cd $OLDPWD
 
 rm -r build
