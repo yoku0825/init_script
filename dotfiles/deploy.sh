cb2e7c7ec4095e0408bc104f3878e1e39079655c
yoku0825 <yoku0825@gmail.com>
2025-01-21 08:15:09 +0000

    Fix

--- dotfiles/deploy.sh
+++ dotfiles/deploy.sh
@@ -44,7 +44,7 @@ function vim_for_perl
 function install_atuin
 {
   curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
-  ln -s $PWD/atuin.config.toml ~/.config/atuin/config.toml
+  ln -sf $PWD/atuin.config.toml ~/.config/atuin/config.toml
 }
 
 if [ "$#" = "0" ] ; then
